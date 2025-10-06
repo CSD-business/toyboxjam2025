7f9e7bd838cde17ce55a8bf5dd4617795855ee87
YakuzaBalooza <jkrow124@gmail.com>
2025-10-06 17:12:47 -0400

    Stamina Gauge Visuals
    - Added texture and color to the Stamina Gauge.

--- toybox-jam/UI/stamina_pb.gd
+++ toybox-jam/UI/stamina_pb.gd
@@ -1,4 +1,4 @@
-extends ProgressBar
+extends TextureProgressBar
 
 var StaminaCharge = 0.5
 
--- toybox-jam/UI/unit_btn.gd
+++ toybox-jam/UI/unit_btn.gd
@@ -8,6 +8,14 @@ func _ready() -> void:
 	$TitleTxt.text = Title
 	$CostTxt.text = str(Cost)
 
+func _process(delta: float) -> void:
+	if GlobalVars.Stamina - Cost < 0:
+		modulate = Color.GRAY
+		$CostTxt.modulate = Color.RED
+	else:
+		modulate = Color.WHITE
+		$CostTxt.modulate = Color.WHITE
+
 func _pressed() -> void:
 	GlobalVars.Pieces.AllyUnitId = UnitId
 	GlobalVars.CurCost = Cost
